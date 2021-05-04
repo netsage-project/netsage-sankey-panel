442750ffb21e84540bf6e39b155c494888de64e1
Biju Jacob <github@biju.com>
2021-05-04 14:36:51 -0700

    Add basic comments and documentation

--- src/module.ts
+++ src/module.ts
@@ -2,6 +2,12 @@ import { PanelPlugin } from '@grafana/data';
 import { NetSageSankeyOptions } from './types';
 import { NetSageSankey } from './NetSageSankey';
 
+/**
+ * Grafana panel plugin main module
+ *
+ * @param {*} { panel: React.ComponentType<PanelProps<NetSageSankeyOptions>> | null }
+ * @return {*} { builder: PanelOptionsEditorBuilder<NetSageSankeyOptions> }
+ */
 export const plugin = new PanelPlugin<NetSageSankeyOptions>(NetSageSankey).setPanelOptions(builder => {
   return builder.addBooleanSwitch({
     path: 'showWarmColors',
